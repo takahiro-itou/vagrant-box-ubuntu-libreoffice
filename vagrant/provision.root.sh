bff967adf609d367fc80d6a1ab85d2b7b545176d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-01-01 10:39:15 +0900

    パッケージをアップグレード

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -6,12 +6,12 @@ sudo  timedatectl  set-timezone Asia/Tokyo
 
 sudo  apt  update
 
-sudo  apt-mark   hold  libssl1.1
-sudo  apt  upgrade -y
-sudo  apt  install -y  build-essential  mercurial
+export DEBIAN_FRONTEND=noninteractive
+sudo  -E  apt  upgrade -y
+sudo  -E  apt  install -y  build-essential  mercurial
 
-sudo  apt  install -y  ubuntu-desktop
-sudo  apt  install -y  qemu
+sudo  -E  apt  install -y  ubuntu-desktop
+sudo  -E  apt  install -y  qemu
 
 # clean up
 sudo  rm -f  /var/lib/apt/lists/lock
