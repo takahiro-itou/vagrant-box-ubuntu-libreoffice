d2ca1f19083230a35426beb37645ebff81ad7c81
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-11 19:27:42 +0900

    リファクタリング

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash  -xue
 
-boxFile='ubuntu-libreoffice.box'
-boxName='ubuntu-focal64-libreoffice'
+box_file='ubuntu-libreoffice.box'
+box_name='ubuntu-focal64-libreoffice'
 
 # Account Name of Vagrant Cloud.
 vcUser='takahiro-itou'
@@ -12,7 +12,7 @@ vagrant  destroy -f  || exit $?
 vagrant  up          || exit $?
 vagrant  halt
 
-vagrant  package   --output ../${boxFile}  default
+vagrant  package   --output ../${box_file}  default
 
 popd
 set  +x
@@ -22,5 +22,5 @@ To add package into box list, run:
 _EOF_
 
 cat  << _EOF_
-vagrant  box  add  --name ${vcUser}/${boxName}  ${boxFile}
+vagrant  box  add  --name ${vcUser}/${box_name}  ${box_file}
 _EOF_
