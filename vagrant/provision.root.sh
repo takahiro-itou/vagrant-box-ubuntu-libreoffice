60a6c20eb2e8007d91ffbabb501c3f066b1bcdf4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-11 19:32:35 +0900

    リファクタリング

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -9,21 +9,38 @@ sudo  apt  update
 export DEBIAN_FRONTEND=noninteractive
 sudo  -E  apt  upgrade -y
 
-sudo  apt  install -y  build-essential                          \
-    cmake  doxygen  emacs  git  mercurial                       \
-    autoconf  automake   clang  gcc  g++                        \
-    libcppunit-dev  libtool  ncurses-dev                        \
-    make  time  sudo                                            \
-    language-pack-ja-base  language-pack-ja                     \
-    fcitx  fcitx-mozc  ibus-mozc
+sudo  apt-get  install -y   \
+    build-essential         \
+    cmake                   \
+    doxygen                 \
+    emacs                   \
+    git                     \
+    mercurial               \
+    autoconf                \
+    automake                \
+    clang                   \
+    gcc                     \
+    g++                     \
+    libcppunit-dev          \
+    libtool                 \
+    ncurses-dev             \
+    make                    \
+    time                    \
+    sudo                    \
+    language-pack-ja-base   \
+    language-pack-ja        \
+    fcitx                   \
+    fcitx-mozc              \
+    ibus-mozc               \
+    ;
 
-sudo  apt  install -y  ubuntu-desktop
-sudo  apt  install -y  qemu
+sudo  apt-get  install -y  ubuntu-desktop
+sudo  apt-get  install -y  qemu
 
 # clean up
-sudo  apt  -y  --purge  autoremove
-sudo  apt  autoclean
-sudo  apt  clean
+sudo  apt-get  -y  --purge  autoremove
+sudo  apt-get  autoclean
+sudo  apt-get  clean
 
 sudo  rm -f  /var/lib/apt/lists/lock
 sudo  rm -f  /var/cache/apt/archives/lock
