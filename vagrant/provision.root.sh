a3c5fecda7765019b42d295616e4f458e92f3ddc
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-11 19:55:24 +0900

    ビルドに必要なパッケージをインストール

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -10,28 +10,61 @@ export DEBIAN_FRONTEND=noninteractive
 sudo  -E  apt  upgrade -y
 
 sudo  apt-get  install -y   \
+    ant                                 \
+    ant-optional                        \
     autoconf                \
     automake                \
+    bison                               \
     build-essential         \
+    ccache                              \
     clang                   \
     cmake                   \
+    default-jdk                         \
     doxygen                 \
+    graphviz                            \
     emacs                   \
     fcitx                   \
     fcitx-mozc              \
+    flex                                \
     gcc                     \
     g++                     \
-    git                     \
+    git                                 \
+    gperf                               \
     ibus-mozc               \
+    junit4                              \
     language-pack-ja-base   \
     language-pack-ja        \
+    libavahi-client-dev                 \
     libcppunit-dev          \
+    libcups2-dev                        \
+    libfontconfig1-dev                  \
+    libgstreamer1.0-dev                 \
+    libgstreamer-plugins-base1.0-dev    \
+    libgtk-3-dev                        \
+    libkf5config-dev                    \
+    libkf5coreaddons-dev                \
+    libkf5i18n-dev                      \
+    libkf5kio-dev                       \
+    libkf5windowsystem-dev              \
+    libkrb5-dev                         \
+    libnss3-dev                         \
     libtool                 \
+    libx11-dev                          \
+    libxml2-utils                       \
+    libxrandr-dev                       \
+    libxslt1-dev                        \
+    libxt-dev                           \
     ncurses-dev             \
     make                    \
     mercurial               \
+    nasm                                \
+    python3                             \
+    python3-dev                         \
+    qtbase5-dev                         \
     sudo                    \
     time                    \
+    xsltproc                            \
+    zip                                 \
     ;
 
 sudo  apt-get  install -y  ubuntu-desktop
